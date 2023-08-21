638025f17830fb013e5cfda5d0e0d54e906af4d4
Allen Efienokwu <train4aws@gmail.com>
2023-08-21 02:14:01 -0400

    Updating main file

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  resource "aws_s3_bucket" "this" {
-    bucket = "train4aws-s3-bucket-demo-2023099"
+    bucket = "train4aws-s3-bucket-demo-${var.environment}"
     tags = {
       Name = "s3-bucket-demo-2023-${var.environment}"
     }
