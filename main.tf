e39f38ef3ab00f10d381fe85d083aa24325fe947
train4aws <79434869+train4aws@users.noreply.github.com>
2023-08-21 00:14:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# resource "aws_s3_bucket" "this" {
-#   bucket = "train4aws-s3-bucket-demo-2023099"
-#   tags = {
-#     Name = "s3-bucket-demo-2023-${var.environment}"
-#   }
-# }
+ resource "aws_s3_bucket" "this" {
+    bucket = "train4aws-s3-bucket-demo-2023099"
+    tags = {
+      Name = "s3-bucket-demo-2023-${var.environment}"
+    }
+  }
